f5fb8ff86e9b2432dd7830f89aa977f2d5882f1a
Vera Scheunert <vera.scheunert@aol.com>
2020-03-30 08:25:05 +0200

    Darkmode second try

---
+++ index.js
@@ -0,0 +1,26 @@
+var nav = document.querySelector("nav");
+
+var expandMobileNavigation = function() {
+  nav.classList.add("mobile-navigation-expanded");
+};
+
+var collapseMobileNavigation = function() {
+  nav.classList.remove("mobile-navigation-expanded");
+};
+
+var expandButton = document.querySelector("#expand-mobile-navigation");
+expandButton.addEventListener("click", expandMobileNavigation);
+
+var collapseButton = document.querySelector("#collapse-mobile-navigation");
+collapseButton.addEventListener("click", collapseMobileNavigation);
+
+var bodyElement = document.querySelector("body");
+var toggleDarkMode = function() {
+  bodyElement.classList.toggle("dark");
+};
+
+var darkModeButton = document.querySelector("#dark-mode-button");
+darkModeButton.addEventListener("click", toggleDarkMode);
+
+var darkModeButtonMobile = document.querySelector("#dark-mode-button-mobile");
+darkModeButtonMobile.addEventListener("click", toggleDarkMode);
